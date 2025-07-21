38a7c5f252186b9294e381b8b59a2539362fbe40
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-07-21 22:46:18 +0700

    Initial Commit

---
+++ GetFitApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GetFitApp
+//
+//  Created by Anthony on 21/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GetFitApp/GetFitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetFitAppApp.swift
+//  GetFitApp
+//
+//  Created by Anthony on 21/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GetFitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
