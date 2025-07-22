75999ec25c335a21b018d2cf0ee19a6be5a4d893
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-07-23 05:45:51 +0700

    Refactoring. Substitute array of exercises with a struct

---
+++ GetFitApp/Exercise.swift
@@ -0,0 +1,8 @@
+//
+//  Exercise.swift
+//  GetFitApp
+//
+//  Created by Anthony on 23/7/25.
+//
+
+import Foundation
---
+++ GetFitApp/Views/TabViewStyleVariation.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewStyleVariation.swift
+//  GetFitApp
+//
+//  Created by Anthony on 23/7/25.
+//
+
+import SwiftUI
+
+struct TabViewStyleVariation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabViewStyleVariation()
+}
