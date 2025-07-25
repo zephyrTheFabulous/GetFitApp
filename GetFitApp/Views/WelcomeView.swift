99ab62ddef9b40251d3a1293b0aaa0bc3c363a27
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-07-25 22:10:47 +0700

    Add "Get Started" button to WelcomView

--- GetFitApp/Views/WelcomeView.swift
+++ GetFitApp/Views/WelcomeView.swift
@@ -1,43 +1,52 @@
-//
-//  WelcomeView.swift
-//  GetFitApp
-//
-//  Created by Anthony on 22/7/25.
-//
+  //
+  //  WelcomeView.swift
+  //  GetFitApp
+  //
+  //  Created by Anthony on 22/7/25.
+  //
 
 import SwiftUI
 
 struct WelcomeView: View {
-    var body: some View {
-      ZStack {
-        VStack {
-          HeaderView(titleName: "Welcome")
+  var body: some View {
+    ZStack {
+      VStack {
+        HeaderView(titleName: "Welcome")
 
-            // History button
-          Spacer()
-          Button("History") {
+          // History button
+        Spacer()
+        Button("History") {
 
-          }
-        } //: VS
-        VStack {
-          HStack (alignment: .top)  {
-            Image("running")
-              .resizable()
-              .scaledToFill()
-              .frame(width: 240, height: 240)
-              .clipShape(Circle())
-            VStack(alignment: .leading) {
-              Text("Get fit")
-                .font(.largeTitle)
-              Text("with high intensity interval training")
-                .font(.headline)
-            }
-          } //: HS
         }
-      } //: ZS
-    }
+      } //: VS
+      VStack {
+        HStack (alignment: .top)  {
+          Image("running")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 240, height: 240)
+            .clipShape(Circle())
+          VStack(alignment: .leading) {
+            Text("Get fit")
+              .font(.largeTitle)
+            Text("with high intensity interval training")
+              .font(.headline)
+          }
+        } //: HS
+        Button {
+
+        } label: {
+          Text("Get Started")
+          Image(systemName: "chevron.right.circle")
+        }
+        .font(.title2)
+        .padding()
+        .background(Capsule().stroke(.gray, lineWidth: 2))
+      } //: VS
+    } //: ZS
+  }
 }
 
 #Preview {
-    WelcomeView()
+  WelcomeView()
 }
